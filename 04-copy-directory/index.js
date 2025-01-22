ddb3444ffc53d9df00fe7e22d78436ee2081082e
LestrSerg <lestrlearn@gmail.com>
2025-01-22 22:26:06 +0300

    fix: correct update of the destination folder when deleting files in the source

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,42 +6,58 @@ function copyDirectory() {
 	const source = path.join(__dirname, 'files');
 	const dest = path.join(__dirname, 'files-copy');
 
-	fs.mkdir(dest, { recursive: true }, (err) => {
-		if (err) {
-    		console.error(`Error creating directory ${dest}:`, err);
-        	return;
-	        }
 
-    	    fs.readdir(source, { withFileTypes: true }, (err, files) => {
-        	if (err) {
-            	 console.error(`Error reading directory ${source}:`, err);
-             	return;
-	        }
-
-    	    for (let i = 0; i < files.length; i++) {
-	    	    const file = files[i];
-				const sourcePath = path.join(source, file.name);
-				const destPath = path.join(dest, file.name);
 	
+	fs.mkdir(dest, { recursive: true, }, (err) => {
+	if (err) {
+   		console.error(`Error creating directory ${dest}:`, err);
+       	return;
+        }
 
-    	    	if (file.isDirectory()) {
-        	    	copyDirectory(sourcePath, destPath);
-            	} else if (file.isFile()) {
-           			fs.copyFile(sourcePath, destPath, (err) => {
-    					if (err) {
-				    		console.log("Error found:", err);
-				
-	    				} else {
-							console.log(`Copied file: ${sourcePath} to ${destPath}`);						
-						}
+		fs.readdir(dest, (err, files) => {
+  		if (err) throw err;
 
-            		});
-		    	
-				}
-	
-    		};
+	  	for (const file of files) {
+	    		fs.unlink(path.join(dest, file), (err) => {
+    	  		if (err) throw err;
+		    	});
+	  		}
 		});
+
+
+
+
+
+	    fs.readdir(source, { withFileTypes: true }, (err, files) => {
+       	if (err) {
+           	 console.error(`Error reading directory ${source}:`, err);
+           	return;
+        }
+
+   	    for (let i = 0; i < files.length; i++) {
+    	    const file = files[i];
+			const sourcePath = path.join(source, file.name);
+			const destPath = path.join(dest, file.name);
+	
+
+   	    	if (file.isDirectory()) {
+       	    	copyDirectory(sourcePath, destPath);
+           	} else if (file.isFile()) {
+       			fs.copyFile(sourcePath, destPath, (err) => {
+   					if (err) {
+			    		console.log("Error found:", err);
+			
+    				} else {
+						console.log(`Copied file: ${sourcePath} to ${destPath}`);						
+					}
+
+           		});
+		    	
+			}
+	
+   		};
 	});
+});
 }
 
 
